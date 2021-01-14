e7e1ff928e8c322184ff5bf902b73649e403fa15
szabomidu <>
2021-01-14 18:16:21 +0100

    Created Connection class which is responsible for creating a new instance of PDO from the provided connection data, and a method called 'getConnection' to return the PDO instance

---
+++ src/Database/Connection/Connection.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Database\Connection;
+
+
+use Exception\DatabaseException;
+use PDO;
+use PDOException;
+
+/**
+ * Class Connection
+ *
+ * Connection class is responsible for creating a new instance of PDO
+ * from the provided connection data.
+ *
+ * @package Database\Connection
+ */
+class Connection
+{
+    private ?PDO $dbConnection = null;
+
+    /**
+     * Connection constructor.
+     *
+     * Creates a new PDO instance representing a connection to a database.
+     *
+     * @throws DatabaseException when connecting to the database fails.
+     */
+    public function __construct()
+    {
+        $database = getenv('DB_DATABASE');
+        $host = getenv('DB_HOST');
+        $port = getenv('DB_PORT');
+        $username = getenv('DB_USERNAME');
+        $password = getenv('DB_PASSWORD');
+
+        try {
+            $this->dbConnection = new PDO("mysql:host=" . $host . ";port=" . $port . ";dbname=" . $database, $username, $password);
+        } catch (PDOException $exception) {
+            $errorMessage = "Error while trying to connect to database!";
+            throw new DatabaseException($errorMessage);
+        }
+    }
+
+    /**
+     * Returns the PDO instance created in the constructor method.
+     *
+     * @return PDO|null
+     */
+    public function getConnection(): ?PDO
+    {
+        return $this->dbConnection;
+    }
+}
