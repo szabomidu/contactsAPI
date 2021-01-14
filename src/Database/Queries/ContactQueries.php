d7a321c70ddaf52835ce11c60acf16ece7babe18
szabomidu <>
2021-01-14 21:30:59 +0100

    Added PHPDoc documentation to class ContactQueries

--- src/Database/Queries/ContactQueries.php
+++ src/Database/Queries/ContactQueries.php
@@ -7,9 +7,22 @@ require "src/Database/Queries/QueryTools/QueryTool.php";
 use PDO;
 use src\Database\Queries\QueryTools\QueryTool;
 
+/**
+ * Class ContactQueries
+ *
+ * Containing queries to be executed on database table contacts.
+ *
+ * @package src\Database\Queries
+ */
 class ContactQueries
 {
-
+    /**
+     * Selects all data from the table where the id matches the variable id passed in as parameter.
+     *
+     * @param PDO|null $PDO instance of PDO
+     * @param int $id id of the desired record in the database
+     * @return mixed result of method queryOne from QueryTool class
+     */
     public function findContactById(?PDO $PDO, int $id)
     {
         $sql = "SELECT name,
@@ -21,6 +34,12 @@ class ContactQueries
         return QueryTool::queryOne($PDO, $sql, ["id" => $id]);
     }
 
+    /**
+     * Select all records from database table contacts.
+     *
+     * @param PDO|null $PDO instance of PDO
+     * @return array result of method queryAll from QueryTool class
+     */
     public function getAllContacts(?PDO $PDO)
     {
         $sql = "SELECT name,
@@ -31,7 +50,14 @@ class ContactQueries
         return QueryTool::queryAll($PDO, $sql);
     }
 
-    public function createNewContact(?PDO $PDO, $input)
+    /**
+     * Inserts data from user input into contacts database table.
+     *
+     * @param PDO|null $PDO instance of PDO
+     * @param $input array associative array containing user input
+     * @return string result of method executeReturningId from QueryTool class
+     */
+    public function createNewContact(?PDO $PDO, array $input)
     {
         $sql = "INSERT INTO contacts(name, email, phone_number, address)
                 VALUES (:name, :email, :phoneNumber, :address)";
@@ -42,7 +68,14 @@ class ContactQueries
                 "address" => $input["address"]]);
     }
 
-    public function updateContact(?PDO $PDO, $input, $id)
+    /**
+     * Updates record in contacts database table where id matches the id passed in as parameter.
+     *
+     * @param PDO|null $PDO instance of PDO
+     * @param $input array associative array containing user input
+     * @param $id integer id of the contact to be updated
+     */
+    public function updateContact(?PDO $PDO, array $input, int $id)
     {
         $sql = "UPDATE contacts 
                 SET email = :email
