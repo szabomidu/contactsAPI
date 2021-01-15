9d9bb916ce49b8ac77c834a50c643f6b986d8139
szabomidu <>
2021-01-15 19:25:44 +0100

    Added PHPDoc to Logger

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -3,26 +3,45 @@
 
 namespace src\Logger;
 
-
+/**
+ * Class Logger
+ *
+ * A basic logger class to write out errors into error.log file.
+ *
+ * @package src\Logger
+ */
 class Logger
 {
     private $loggerFile;
 
     /**
      * Logger constructor.
+     *
+     * Sets the path to the logger file from environment variable.
      */
     public function __construct()
     {
         $this->loggerFile = $_ENV["LOGGER_FILE"];
     }
 
-    public function logError($errorMessage)
+    /**
+     * Calls composeLoggerMessage and writeToLoggerFile methods. This method
+     * is called when an error occurs and logging is called.
+     *
+     * @param $errorMessage string error message in string format
+     */
+    public function logError(string $errorMessage)
     {
         $content = $this->composeLoggerMessage($errorMessage);
         $this->writeToLoggerFile($content);
     }
 
-    private function writeToLoggerFile($content)
+    /**
+     * Writes content to logger file in append mode.
+     *
+     * @param $content string composed logger message
+     */
+    private function writeToLoggerFile(string $content)
     {
         $file = fopen($this->loggerFile, "a");
         fwrite($file, $content);
@@ -30,7 +49,13 @@ class Logger
         fclose($file);
     }
 
-    private function composeLoggerMessage($errorMessage): string
+    /**
+     * This method adds date and time and '[ERROR]' prefix to error message.
+     *
+     * @param $errorMessage string error message in string format
+     * @return string composed logger message
+     */
+    private function composeLoggerMessage(string $errorMessage): string
     {
         return "[ERROR]" . date('Y-m-d H:i:s') . $errorMessage;
     }
